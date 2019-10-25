951655c57264aa8cf660dcc7b09851f2ffba6b09
Kemal Kurniawan <kemal.kurniawan@student.unimelb.edu.au>
2019-10-25 15:37:08 +1100

    Add docstring to find_minimum_branching

--- pyedmond/__init__.py
+++ pyedmond/__init__.py
@@ -1,7 +1,24 @@
+from typing import List, Optional, Tuple
+
 from ._core import build_graph, minimum_branching
 
 
-def find_minimum_branching(n_nodes, edges, roots=None):
+def find_minimum_branching(
+        n_nodes: int, edges: List[Tuple[int, int, float]],
+        roots: Optional[List[int]] = None) -> List[Tuple[int, int]]:
+    """Find minimum branching of a directed graph with Edmond's algorithm.
+
+    See https://en.wikipedia.org/wiki/Edmonds%27_algorithm.
+
+    Args:
+        n_nodes: Number of nodes in the graph.
+        edges: List of edges in the graph. An edge is given as a tuple ``(u, v, w)`` which
+            means there is an edge from node ``u`` to node ``v`` with weight ``w``.
+        roots: List of nodes to be the root(s) of the resulting branching.
+
+    Returns:
+        The minimum branching.
+    """
     if roots is None:
         roots = []
 
